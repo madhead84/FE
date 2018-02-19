5f8ec95fe2a151d5d5ac8ab126baa98a3971ee2d
madhead84 <mad_head@ua.fm>
2018-02-19 13:18:03 +0200

    comit

---
+++ hello-world/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Article from './Article'
+import articles from '../fixtures'
+
+function App() {
+    return <div>
+        <h1>App name</h1>
+        <Article article={articles[0]} foo = "bar" flag={true}/>
+    </div>
+}
+
+export default App
