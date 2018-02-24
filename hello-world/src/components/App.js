b05aa4aa0919f30f65dd020091bc6f3fc63325d6
madhead84 <mad_head@ua.fm>
2018-02-24 14:46:00 +0200

    Added components

--- hello-world/src/components/App.js
+++ hello-world/src/components/App.js
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ArticleList from './ArticleList'
 import articles from '../fixtures'
 import "bootstrap/dist/css/bootstrap.css"
 
-function App() {
+class App extends Component {
+    state = {
+        reverted: false
+    }
+
+    render(){
     return (
         <div className="container">
             <div className="jumbotron">
-                <h1 className="display-3">App name</h1>
+                <h1 className="display-3">
+                    App name
+                    <button className="btn" onClick={this.revert}>Revert</button>
+                </h1>
             </div>
         <h1>App name</h1>
-        <ArticleList articles={articles}/>
+        <ArticleList articles={this.state.reverted ? articles.reverse() : articles}/>
     </div>
     )
 }
-
+    revert = () => this.setState({
+        reverted: !this.state.reverted
+    })
+}
 export default App
