c7bff5fbb88cab61bc9022d765bd27ecc4fdb70c
madhead84 <mad_head@ua.fm>
2018-04-13 16:16:25 +0300

    check form added

--- FE/testJS/.idea/inspectionProfiles/test/test.js
+++ FE/testJS/.idea/inspectionProfiles/test/test.js
@@ -6,13 +6,6 @@ function min(a, b) {
 }
 
 function min(a, b) {
-    var c;
-    c = a - b;
-    if (c < 0) return a;
-    else if (c > 0) return b;
-}
-
-function min(a, b) {
     if (a < b) {
         return a;
     }
@@ -31,4 +24,40 @@ function generateRandomArray(minNumberOfArrey, maxNumberOfArray, arrayLeng) {
         randomArray.push(rand);
     }
     return randomArray;
+}
+
+var myobject = { value : 0,
+    increment: function (inc) {
+    this.value += typeof inc === 'number' ? inc : 1
+
+    }
+
+}
+
+function generateRandomNumberFrom1to6() {
+    return Math.round(0.5 + Math.random()*6);
+}
+
+function anagram(word1 , word2){
+    var word1ToArray = word1.toUpperCase().split('').sort().toString();
+    var word2ToArray = word2.toUpperCase().split('').sort().toString();
+    return (word1ToArray == word1ToArray) ? true : false;
+}
+
+function checkForm() {
+    var documentsForm = document.forms[0];
+    var documentsElements = documentsForm.elements;
+    var isEmpty = false;
+    for (var i = 0; i<documentsElements.length; i++){
+        if(documentsElements[i].type = "text"){
+            if(documentsElements[i].value == ""){
+                documentsElements[i].style.borderColor = "red";
+                isEmpty = true;
+            } else {
+                documentsElements[i].style.borderColor = "";
+            }
+        }
+    }
+    if(isEmpty) alert("Fill all fields");
+    else documentsForm.submit();
 }
